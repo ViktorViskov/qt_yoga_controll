0471b03855c739b1337e8750382a60a2548ecbbf
Viktor Viskov <carrergt@gmail.com>
2022-09-17 10:10:08 +0200

    Optimized version

--- batt_tray.py
+++ batt_tray.py
@@ -4,31 +4,30 @@ from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
 from time import sleep
 from os import popen
-from os.path import exists
+from os.path import exists, expanduser
 
 # worker for update battery icon
 class batt_worker(QThread):
     out = pyqtSignal(str, bool)
-    def  __init__(self, parent = None):
+    path: str
+    def  __init__(self, path: str ,parent = None):
         QThread.__init__(self, parent=parent)
+        self.path = path
 
     # worker body
     def run(self):
       while True:
-        # result = popen("upower -i /org/freedesktop/UPower/devices/DisplayDevice | awk '/percentage/' | awk '{print $2}'").read().strip()
+        # predefine variables
+        mode = False
+        level = "0"
+
         # try to get batt status
-        if exists("/sys/class/power_supply/BAT1/capacity"):
-            mode = open("/sys/class/power_supply/BAT1/capacity", "r").read()
-            # check for chargering
+        if exists("%s/status" % (self.path)) and open("%s/status" % (self.path), "r").read().strip() == "Charging":
             mode = True
-        else:
-            mode = False
 
         # try to get batt level
-        if exists("/sys/class/power_supply/BAT1/capacity"):
-            level = open("/sys/class/power_supply/BAT1/capacity", "r").read()
-        else:
-            level = "0"
+        if exists("%s/capacity" % (self.path)):
+            level = open("%s/capacity" % (self.path), "r").read().strip()
 
         # send signal
         self.out.emit(level, mode)
@@ -41,13 +40,11 @@ class batt_icon(QIcon):
         # preparing
         pixmap = QPixmap(128, 128)
         pixmap.fill(QColor(0,0,0,0))
-
-        # painter
         painter = QPainter(pixmap)
 
         # if charging add lighter
         if mode:
-            charg_pen = QPen(QColor(255,255,0,255))
+            charg_pen = QPen(QColor(0,0,0,255),3)
             charg_brush = QBrush(QColor(255,255,0,255))
             painter.setPen(charg_pen)
             painter.setBrush(charg_brush)
@@ -63,9 +60,9 @@ class batt_icon(QIcon):
             color = QColor(127,0,0,255)
         elif number <= 20:
             color = QColor(255,0,0,255)
-        elif number <= 35:
+        elif number <= 30:
             color = QColor(255,127,0,255)
-        elif number <= 50:
+        elif number <= 40:
             color = QColor(150,150,0,255)
         else:
             color = QColor(0,127,0,255)
@@ -97,15 +94,14 @@ class batt_icon(QIcon):
         if len(bat_status) == 1:
             painter.drawText( QPoint(44, 83), bat_status)
 
+        # for 2 char
         if len(bat_status) == 2:
-            # for 2 char
             painter.drawText( QPoint(25, 83), bat_status)
 
+        # for 3 chars
         if len(bat_status) == 3:
-            # for 3 chars
             painter.drawText( QPoint(10, 83), bat_status)
         
-
         # stop drawing
         painter.end()
         self.addPixmap(pixmap)
@@ -115,7 +111,6 @@ class batt_window(QWidget):
     layout: QVBoxLayout
     button: QPushButton
     label: QLabel
-    mousepos = QCursor.pos()
 
     def __init__(self, parent) -> None:
         # init widget
@@ -131,31 +126,33 @@ class batt_window(QWidget):
 
         # input field
         self.input_field = QLineEdit()
-        self.input_field.setText("/sys/class/power_supply/BAT1/")
+        self.input_field.setText(self.parent.path)
         self.layout.addWidget(self.input_field)
 
         # save button
         self.button = QPushButton("Save")
-        self.button.mousePressEvent = lambda event: print(self.input_field.text())
+        self.button.mousePressEvent = lambda event: self.save_event()
         self.layout.addWidget(self.button)
 
-        # if exist saved position, move to this position
-        # if self.parent.pos:
-            # self.move(self.parent.pos)
         # self.setWindowFlags(Qt.WindowType.SplashScreen | Qt.WindowType.WindowStaysOnTopHint | Qt.WindowType.FramelessWindowHint)
-        self.setWindowFlags(  Qt.WindowType.Dialog)
+        self.setWindowFlags(Qt.WindowType.Dialog)
 
+    def save_event(self):
+        self.parent.save_config(self.input_field.text())
+        self.parent.start_worker()
 
-    def change_label(self):
-        result = popen("upower -i /org/freedesktop/UPower/devices/DisplayDevice | awk '/percentage/' | awk '{print $2}'").read()
+        # close window
+        self.close()
 
 # main class for tray app
 class batt_tray(QSystemTrayIcon):
     # varaibles
     app: QApplication
     pos: QPoint = None
-    battery_level = ""
-    b_worker: batt_worker
+    battery_level: str = ""
+    battery_status: bool = False
+    b_worker: batt_worker = None
+    path: str = "/sys/class/power_supply/BAT1/"  #default path 
 
     # widgets
     menu: QMenu
@@ -168,22 +165,47 @@ class batt_tray(QSystemTrayIcon):
         super().__init__()
         self.app = app
 
-        # config tray icon and create worker
-        self.setIcon(batt_icon("0", False))
-        self.b_worker = batt_worker(self)
-        self.b_worker.out.connect(self.update_battery_icon)
-        self.b_worker.start()
+        # load config file
+        self.load_config()
 
+        # config tray icon
+        self.setIcon(batt_icon("0", False))
         self.setVisible(True)
 
-        # create menu
-        self.create_menu()
+        # start worker
+        self.start_worker()
 
-        # adding menu
+        # create menu and add menu
+        self.create_menu()
         self.setContextMenu(self.menu)
 
         # show window on click
         self.activated.connect(self.show_status)
+
+    # method for load user config file
+    def load_config(self):
+        user_directory = expanduser("~")
+        if exists("%s/.config/pybatticon.conf" % user_directory):
+            self.path = open("%s/.config/pybatticon.conf" % user_directory, "r").read()
+
+    # method for save to config file
+    def save_config(self, config_string:str):
+        user_directory = expanduser("~")
+        open("%s/.config/pybatticon.conf" % user_directory, "w").write(config_string)
+        self.path = config_string
+
+    
+    # method for start and restart worker
+    def start_worker(self):
+        # terminate worker if exist
+        if self.b_worker:
+            self.b_worker.terminate()
+            self.b_worker = None
+
+        # create new worker
+        self.b_worker = batt_worker(self.path, self)
+        self.b_worker.out.connect(self.update_battery_icon)
+        self.b_worker.start()
 
     # method for create menu
     def create_menu(self):
@@ -199,47 +221,43 @@ class batt_tray(QSystemTrayIcon):
         if key_event == 3:
             # check for window is show
             if not self.window:
-                if not self.pos: 
-                    #  define window size
-                    # self.pos = QCursor.pos()
-                    pass
-                #     window_size = QDesktopWidget().screenGeometry()
-                #     self.pos = QPoint(window_size.width() - (window_size.width() - 300),window_size.height() - (window_size.height() - 100))
                 self.window = batt_window(self)
                 self.window.show()
                 self.window.closeEvent = lambda event: self.show_status(3)
             else:
-                self.pos = self.window.pos()
                 self.window.closeEvent = None
                 self.window.close()
                 self.window = None
         
-        elif key_event == 4:
-            self.app_quit()
+        # elif key_event == 4:
+        #     self.app_quit()
 
 
     # method for update buttery icon
     def update_battery_icon(self, new_battery_level: str, mode: bool):
         # check for new data
-        if self.battery_level != new_battery_level:
+        if self.battery_level != new_battery_level or self.battery_status != mode:
             # change icon
             self.setIcon(batt_icon(new_battery_level, mode))
             
             # set info about time to empty
-            message = popen("upower -i /org/freedesktop/UPower/devices/DisplayDevice | awk '/percentage/'").read().strip()
+            out = popen("upower -i /org/freedesktop/UPower/devices/DisplayDevice | awk '/time to /'").read().strip().capitalize().split(":")
 
-            # check for message is exist
-            if not message:
+            # message preparing
+            if out:
+                message = "%s: %s" % (out[0].strip(),out[1].strip())
+            else:
                 message = "Can not read data about battery"
             
             self.setToolTip(message)
 
         # replace battery level
         self.battery_level = new_battery_level
+        self.battery_status = mode
 
     # method for safe close app
     def app_quit(self):
-        self.b_worker.terminate()
         self.b_worker.finished.connect(self.app.quit)
+        self.b_worker.terminate()
 
 
